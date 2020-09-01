86e11fe22b2580c9d08d26d92201d191f4485cd3
raotnameh <raotnameh@gmail.com>
2020-09-02 04:28:06 +0530

    -

--- data/usen.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-f = open("/home/hemant/Robust_ASR/data/dev_sorted.csv","r")
-
-de = f.readlines()
-#print(de)
-lst = [[i.strip()] for i in de if i.split(",")[2].strip() == "EN" or i.split(",")[2].strip() =="US"]
-df = pd.DataFrame(lst)
-df.to_csv("/home/hemant/Robust_ASR/data/dev_sorted_EN_US.csv",index=False, header=False)
-
--- dummy_Asr/unicode_hindi.py
+++
@@ -1,5 +0,0 @@
-import json
-with open('/media/data_dump/hemant/harsh/sopi_deep/out.txt' , "r") as f:
-         out = f.read()
-out = json.loads(out)
-print(out)
