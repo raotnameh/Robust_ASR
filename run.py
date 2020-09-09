fb34786d1f0edbff11fdeca7f6e19054c60f06f1
raotnameh <raotnameh@gmail.com>
2020-09-09 20:04:26 +0530

    -

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 def run(alphaValue,cnt):
-    command = "python train.py --enco-modules 2 --dummy --enco-res --train-manifest data/csvs/train_sorted_EN_US.csv --val-manifest data/csvs/dev_sorted.csv --cuda --rnn-type gru --hidden-layers 3 --momentum 0.94 --opt-level O1 --loss-scale 1.0 --hidden-size 1024 --epochs 100 --lr 0.0001 --batch-size 24 --gpu-rank 1 --dummy  --checkpoint --update-rule 4 --mw-beta 0.1 --mw-gamma 0.1 --disc-modules 1 --disc-res --exp-name /media/data_dump/hemant/rachit/Robust_ASR/save/train_Asr_experimentation/experiment_1/exp_"+str(cnt) +" --mw-alpha " + str(alphaValue)
+    command = "python train.py --enco-modules 2 --enco-res --train-manifest data/csvs/train_sorted.csv --val-manifest data/csvs/dev_sorted.csv --cuda --rnn-type gru --hidden-layers 5 --opt-level O1 --loss-scale 1.0 --hidden-size 1024 --epochs 25 --lr 0.001 --batch-size 32 --gpu-rank 4 --update-rule 1 --exp-name /media/data_dump/hemant/rachit/Robust_ASR/save/train_Asr_experimentation/experiment_1/exp_"+str(cnt) +" --mw-alpha " + str(alphaValue) + " --train-asr"
 
     os.system(command)
 
