c2524f1cd4a3995aa643c3c3350ac6d383edfc58
raotnameh <raotnameh@gmail.com>
2021-01-08 00:22:17 +0530

    updated

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ batch = 20
 
 train_ = "data/csvs/train7_sorted.csv"
 test_ = "data/csvs/dev4_sorted.csv"
-gpu_rank = 0
+gpu_rank = "0,1,2,3,4"
 def run(beta,gamma,save):
-    command = f"python {file}.py --enco-modules 2 --enco-res --train-manifest {train_} --val-manifest {test_} --cuda --rnn-type gru --hidden-layers 5 --hidden-size 1024 --epochs 50 --lr 0.001 --batch-size {batch} --checkpoint --gpu-rank {gpu_rank} --update-rule 2 --exp-name exp2/{save} --mw-alpha 0.0001 --mw-beta {beta} --mw-gamma {gamma} --forg-modules 2 --forg-res --num-epochs 10 --patience 10"
+    command = f"horovodrun -np 5 -H localhost:5 python {file}.py --enco-modules 2 --enco-res --train-manifest {train_} --val-manifest {test_} --cuda --rnn-type gru --hidden-layers 5 --hidden-size 1024 --epochs 50 --lr 0.001 --batch-size {batch} --checkpoint --gpu-rank {gpu_rank} --update-rule 2 --exp-name exp2/{save} --mw-alpha 0.0001 --mw-beta {beta} --mw-gamma {gamma} --forg-modules 2 --forg-res --num-epochs 10 --patience 10"
 
     print('\n\n',command,'\n\n')
     os.system(command)
