9a315f0641ab35e1fc4db90f0ce359480c2eb3b4
raotnameh <raotnameh@gmail.com>
2021-02-07 15:29:44 +0530

    updated model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 
 class block_B(nn.Module):
-    def __init__(self, sub_blocks, kernel_size=11, dilation=1, stride=1, in_channels=32, out_channels=256, dropout=0.2, nonlinear=2):
+    def __init__(self, sub_blocks, kernel_size=11, dilation=1, stride=1, in_channels=32, out_channels=256, dropout=0.2,batch_norm=True):
         super(block_B, self).__init__()
 
         self.kernel_size = kernel_size
@@ -15,13 +15,16 @@ class block_B(nn.Module):
         self.layers = nn.ModuleList()
         if sub_blocks == 1:
             self.layers.append(
-                nn.Sequential(
                     nn.Conv1d(in_channels, out_channels, kernel_size=self.kernel_size, stride=stride, padding=self.padding, dilation=dilation),
-                    nn.BatchNorm1d(out_channels),
-                    nn.ReLU(),
-                    nn.Dropout(p=dropout),
-                        )
             )
+            if batch_norm:
+                self.layers.append(
+                        nn.Sequential(
+                        nn.BatchNorm1d(out_channels),
+                        nn.Dropout(p=dropout),
+                        nn.ReLU()
+                    )
+                )
         else:
             for i in range(sub_blocks - 1):
                 if i == 0:
@@ -31,17 +34,8 @@ class block_B(nn.Module):
                             nn.BatchNorm1d(out_channels),
                             nn.ReLU(),
                             nn.Dropout(p=dropout),
-                                )
-                    )
-                    for i in range(nonlinear):
-                        self.layers.append(
-                            nn.Sequential(
-                                nn.Conv1d(out_channels, out_channels, kernel_size=1, stride=1,),
-                                nn.BatchNorm1d(out_channels),
-                                nn.ReLU(),
-                                nn.Dropout(p=dropout),
-                            )
                         )
+                    )
                 else: 
                     self.layers.append(
                         nn.Sequential(
@@ -49,17 +43,9 @@ class block_B(nn.Module):
                             nn.BatchNorm1d(out_channels),
                             nn.ReLU(),
                             nn.Dropout(p=dropout),
-                                )
-                    )
-                    for i in range(nonlinear):
-                        self.layers.append(
-                            nn.Sequential(
-                                nn.Conv1d(out_channels, out_channels, kernel_size=1, stride=1,),
-                                nn.BatchNorm1d(out_channels),
-                                nn.ReLU(),
-                                nn.Dropout(p=dropout),
-                            )
                         )
+                    )
+
             self.layers.append(
                 nn.Sequential(
                     nn.Conv1d(out_channels, out_channels, kernel_size=self.kernel_size, stride=stride, padding=self.padding, dilation=dilation),
@@ -69,20 +55,17 @@ class block_B(nn.Module):
             self.conv = nn.Sequential(
                 nn.Conv1d(in_channels, out_channels, kernel_size=self.kernel_size, stride=stride, padding=self.padding, dilation=dilation),
                 nn.BatchNorm1d(out_channels),
-                )
+                )        
             self.last = nn.Sequential(
-                nn.ReLU(),
                 nn.Dropout(p=dropout),
-                )
-        
-
+                nn.ReLU())
     def forward(self, x, lengths):
-        y = x # 32,1,148
+        y = x # 32,161,148 # (batch_size, channels, seq_len)
         for i in range(len(self.layers)):
             y, lengths = self.maskconv1d(self.layers[i](y), lengths, self.stride)
         if self.sub_blocks != 1: 
             y, lengths = self.maskconv1d(self.last(y + self.conv(x)), lengths, self.stride)
-        return y, lengths #lengths
+        return y, lengths 
 
     def maskconv1d(self,x,lengths, stride):
         lengths = (lengths/stride + 0.5).to(lengths.dtype)
@@ -93,8 +76,6 @@ class block_B(nn.Module):
         
         del mask, idxs, max_len 
         return x, lengths
-
-        
 
 
 class block_Deco(nn.Module):
@@ -164,33 +145,19 @@ class block_Deco(nn.Module):
         return y 
 
 
-class InferenceBatchSoftmax(nn.Module):
-    def forward(self, input_):
-        if not self.training:
-            return F.softmax(input_, dim=-1)
-        else:
-            return input_
 
-class Encoder(nn.Module):
-    def __init__(self,in_channels,info, classes = 2, adaptive_pooling=False,transpose=False,softmax=False):
-        super(Encoder, self).__init__()
 
-        self.pooling = adaptive_pooling
-        self.softmax = softmax
-        self.transpose = transpose
-        if self.pooling: 
-            self.adaptive_pooling = nn.AdaptiveAvgPool1d((2))
-            self.linear_1 = torch.nn.Linear(info[-1]['out_channels']*2, classes)
-            self.sigmoid = nn.Sigmoid()
-        
-        self.inference_softmax = InferenceBatchSoftmax()
+
+class Pre(nn.Module):
+    def __init__(self,in_channels,info):
+        super(Pre, self).__init__()
 
         self.layers = nn.ModuleList()
         for i in range(len(info)):
             self.layers.append(
                 block_B(info[i]['sub_blocks'], kernel_size=info[i]['kernel_size'], dilation=info[i]['dilation'],
-                    stride=info[i]['stride'], in_channels=in_channels, nonlinear=info[i]['nonlinear'],
-                    out_channels=info[i]['out_channels'], dropout=info[i]['dropout'],
+                    stride=info[i]['stride'], in_channels=in_channels,
+                    out_channels=info[i]['out_channels'], dropout=info[i]['dropout'],batch_norm=info[i]['batch_norm'],
                     )
             )
             in_channels = info[i]['out_channels']
@@ -198,38 +165,71 @@ class Encoder(nn.Module):
     def forward(self, x, lengths):
         for i in range(len(self.layers)):
             # print(i, "-------",x.shape)
-            x, lengths = self.layers[i](x, lengths,)
-        if self.pooling: 
-            x = self.adaptive_pooling(x)
-            x = self.sigmoid(self.linear_1(torch.flatten(x, start_dim=1)))
-        if self.transpose: x = x.permute(0,2,1)
-        if self.softmax: x = self.inference_softmax(x)
-        #     if transpose: return x.transpose(0,1), lengths
-        #     return x, lengths
-        # if transpose: return x.transpose(0,1), lengths
+            x, lengths = self.layers[i](x, lengths,)    
         return x, lengths
 
+class Encoder(nn.Module):
+    def __init__(self,in_channels,info):
+        super(Encoder, self).__init__()
+        
+        self.layers = nn.ModuleList()
+        for i in range(len(info)):
+            self.layers.append(
+                block_B(info[i]['sub_blocks'], kernel_size=info[i]['kernel_size'], dilation=info[i]['dilation'],
+                    stride=info[i]['stride'], in_channels=in_channels,
+                    out_channels=info[i]['out_channels'], dropout=info[i]['dropout'],batch_norm=info[i]['batch_norm'],
+                    )
+            )
+            in_channels = info[i]['out_channels']
+
+    def forward(self, x, lengths):
+        for i in range(len(self.layers)):
+            # print(i, "-------",x.shape)
+            x, lengths = self.layers[i](x, lengths,)
+        return x, lengths 
 
 class Decoder(nn.Module):
     def __init__(self,in_channels,info):
         super(Decoder, self).__init__()
-
+        
         self.layers = nn.ModuleList()
         for i in range(len(info)):
             self.layers.append(
-                block_Deco(info[i]['sub_blocks'], kernel_size=info[i]['kernel_size'], dilation=info[i]['dilation'],
+                block_B(info[i]['sub_blocks'], kernel_size=info[i]['kernel_size'], dilation=info[i]['dilation'],
                     stride=info[i]['stride'], in_channels=in_channels,
-                    out_channels=info[i]['out_channels'], dropout=info[i]['dropout'],
+                    out_channels=info[i]['out_channels'], dropout=info[i]['dropout'],batch_norm=info[i]['batch_norm'],
                     )
             )
             in_channels = info[i]['out_channels']
-        
-    def forward(self, x,):
+
+    def forward(self, x, lengths):
         for i in range(len(self.layers)):
             # print(i, "-------",x.shape)
-            x = self.layers[i](x)
+            x, lengths = self.layers[i](x, lengths,)
+        # x = F.sigmoid(x)
+        return x.permute(0,2,1), lengths # batch_size, seq_length,classes
+
+
+# class Decoder(nn.Module):
+#     def __init__(self,in_channels,info):
+#         super(Decoder, self).__init__()
+
+#         self.layers = nn.ModuleList()
+#         for i in range(len(info)):
+#             self.layers.append(
+#                 block_Deco(info[i]['sub_blocks'], kernel_size=info[i]['kernel_size'], dilation=info[i]['dilation'],
+#                     stride=info[i]['stride'], in_channels=in_channels,
+#                     out_channels=info[i]['out_channels'], dropout=info[i]['dropout'],
+#                     )
+#             )
+#             in_channels = info[i]['out_channels']
+        
+#     def forward(self, x,):
+#         for i in range(len(self.layers)):
+#             # print(i, "-------",x.shape)
+#             x = self.layers[i](x)
             
-        return x
+#         return x
 
 
 def get_param_size(model):
@@ -245,46 +245,61 @@ if __name__ == '__main__':
     
     import json
 
-    device = torch.device("cuda:1")
+    device = torch.device("cpu")
     with open('labels.json') as label_file:
         labels = str(''.join(json.load(label_file)))
     from config import *
+
+    from torch.utils.tensorboard import SummaryWriter
+
+    # default `log_dir` is "runs" - we'll be more specific here
+    writer = SummaryWriter('experiment_1')
     
-    pre = Encoder(1,configPre()).to(device)
-    # print(pre)
+    pre = Encoder(161,configPre()).to(device)
+    print(pre)
     print(f"Number of parameters in preprocessor is: {get_param_size(pre)/1000000}")
 
     encoder = Encoder(configPre()[-1]['out_channels'],configE()).to(device)
-    # print(encoder)
+    print(encoder)
     print(f"Number of parameters in encoder is: {get_param_size(encoder)/1000000}")
 
     # forget_net = Encoder(configPre()[-1]['out_channels'],configF()).to(device)
     # # print(forget_net)
     # print(f"Number of parameters in forget net is: {get_param_size(forget_net)/1000000}")
 
-    # decoder = Decoder(configE()[-1]['out_channels'],configR()).to(device)
-    # # print(decoder)
-    # print(f"Number of parameters in decoder is: {get_param_size(decoder)/1000000}")
+    decoder = Decoder(configE()[-1]['out_channels'],configDecoder()).to(device)
+    print(decoder)
+    print(f"Number of parameters in decoder is: {get_param_size(decoder)/1000000}")
 
     # discriminator = Encoder(configE()[-1]['out_channels'],configD(), adaptive_pooling=True).to(device)
     # # print(discriminator)
     # print(f"Number of parameters in discriminator is: {get_param_size(discriminator)/1000000}")
 
-    asr = Encoder(configE()[-1]['out_channels'],configP(),transpose=True).to(device)
-    # print(asr)
-    print(f"Number of parameters in asr is: {get_param_size(asr)/1000000}")
+    # asr = Encoder(configE()[-1]['out_channels'],configP(),transpose=True).to(device)
+    # # print(asr)
+    # print(f"Number of parameters in asr is: {get_param_size(asr)/1000000}")
 
     # exit()
     # for 15 second audios a batch size of 14 works
-    B_SZ = 4
+    B_SZ = 1
 
-    X = torch.rand((B_SZ, 1, 1610)).to(device)
+    X = torch.rand((B_SZ, 161, 1610)).to(device)
     WIDTHS = torch.LongTensor(B_SZ).random_(1600,1610 ).to(device)
 
     Z1, L = pre(X, lengths=WIDTHS)  
     print("PRE OUT", Z1.shape)  
-    Z, L = encoder(Z1, lengths=L)    
-    print("ENCODER OUT", Z.shape)
+    Z2, L = encoder(Z1, lengths=L)    
+    print("ENCODER OUT", Z2.shape)
+    Z, L = decoder(Z2,lengths=L)
+    for i in Z:
+        for j in i:
+            print(j)
+    print("DECODER OUT",Z.shape)
+
+    # writer.add_graph(encoder,(Z1,L))
+    # writer.close()
+    exit()
+
     # M, L_ = forget_net(Z1, lengths=WIDTHS)    
     # print("FORGET-NET OUT", M.shape)
     
