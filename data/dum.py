7f7ed50793b475ec9473e1fc1b34d0709fccd690
raotnameh <hemantya@iiitd.ac.in>
2021-06-02 04:42:29 +0530

    updated

---
+++ data/dum.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+from tempfile import NamedTemporaryFile
+
+import librosa
+import numpy as np
+from scipy.io.wavfile import read
+
+def load_audio(path):
+    sample_rate, sound = read(path)
+    sound = sound.astype('float32') / 32767  # normalize audio
+    if len(sound.shape) > 1:
+        if sound.shape[1] == 1:
+            sound = sound.squeeze()
+        else:
+            sound = sound.mean(axis=1)  # multiple channels, average
+    sound = 2*((sound - sound.min()) / (sound.max() - sound.min())) - 1
+    return sound
+
+def get_audio_length(path):
+    output = subprocess.check_output(['soxi -D \"%s\"' % path.strip()], shell=True)
+    return float(output)
+
+
+def audio_with_sox(path, sample_rate, start_time, end_time):
+    """
+    crop and resample the recording with sox and loads it.
+    """
+    with NamedTemporaryFile(suffix=".wav") as tar_file:
+        tar_filename = tar_file.name
+        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} trim {} ={} >/dev/null 2>&1".format(path, sample_rate,
+                                                                                               tar_filename, start_time,
+                                                                                               end_time)
+        os.system(sox_params)
+        y = load_audio(tar_filename)
+        return y
+
+
+def augment_audio_with_sox(path, sample_rate, tempo, gain):
+    """
+    Changes tempo and gain of the recording with sox and loads it.
+    """
+    with NamedTemporaryFile(suffix=".wav") as augmented_file:
+        augmented_filename = augmented_file.name
+        sox_augment_params = ["tempo", "{:.3f}".format(tempo), "gain", "{:.3f}".format(gain)]
+        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} {} >/dev/null 2>&1".format(path, sample_rate,
+                                                                                      augmented_filename,
+                                                                                      " ".join(sox_augment_params))
+        os.system(sox_params)
+        y = load_audio(augmented_filename)
+        return y
+
+def load_randomly_augmented_audio(path, sample_rate=16000, tempo_range=(0.8, 1.2),
+                                  gain_range=(-6, 8)):
+    """
+    Picks tempo and gain uniformly, applies it to the utterance by using sox utility.
+    Returns the augmented utterance.
+    """
+    low_tempo, high_tempo = tempo_range
+    tempo_value = np.random.uniform(low=low_tempo, high=high_tempo)
+    low_gain, high_gain = gain_range
+    #gain_value = np.random.uniform(low=low_gain, high=high_gain)
+    audio = augment_audio_with_sox(path=path, sample_rate=sample_rate,
+                                   tempo=tempo_value, gain=gain_value)
+    return audio
+
+from tqdm.auto import tqdm 
+for i in tqdm(range(10000)):
+    load_randomly_augmented_audio("/home/hemant/accent_data/train7/wav/sample-101535.wav", 16000)
