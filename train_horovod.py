076af42b86d3644fe49da2ea572ea0e07aced97a
raotnameh <hemantya@iiitd.ac.in>
2021-06-04 04:51:41 +0530

    updated

--- train_horovod.py
+++ train_horovod.py
@@ -595,7 +595,7 @@ if __name__ == '__main__':
         for i in models:
             for g in models[i][-1].param_groups:
                 if dummy_lr is None: dummy_lr = g['lr']
-                if g['lr'] >= 1e-4:
+                if g['lr'] >= 1e-8:
                     g['lr'] = g['lr'] * args.learning_anneal
             print(f"Learning rate annealed to: {g['lr']} from {dummy_lr}")
         dummy_lr = None
