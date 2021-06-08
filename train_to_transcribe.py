b6fdcf9cee62870a086d9f6f34b20908004bb65f
raotnameh <hemantya@iiitd.ac.in>
2021-06-08 12:38:41 +0530

    updated

--- train_to_transcribe.py
+++ train_to_transcribe.py
@@ -13,7 +13,8 @@ package = torch.load(args.path, map_location='cpu')
 models = package['models']
 del models['decoder']
 for i in models:
-    models[i] = models[i][0].half()
+    if args.half: models[i] = models[i][0].half()
+    else: models[i] = models[i][0].half()
 print(models.keys())
 updated_package = {'models': models, 'audio_conf': package['audio_conf'], 'labels': package['labels']}
 torch.save(updated_package,args.save)
